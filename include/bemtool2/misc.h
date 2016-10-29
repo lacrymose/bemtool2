a233b0a6893cd4801aab2eb34d6b642491c8c391
Pierre Marchand <pierremarchand@MacBook-Pro-de-Pierre.local>
2016-10-29 18:52:30 +0200

    Modif progress bar => jusqu a 100 maintenant

--- include/bemtool2/misc.h
+++ include/bemtool2/misc.h
@@ -6,8 +6,8 @@
 //  Copyright 2015 Xavier Claeys
 //
 //  bemtool is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by	
-//  the Free Software Foundation, either version 3 of the License, or 
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
 //  bemtool is distributed in the hope that it will be useful,
@@ -24,45 +24,42 @@
 #include <ctime>
 
 class progress{
-  
- private:
-  const char* title;
-  const int length;
-  int       prg;
-  int       it;
-  clock_t   t0;
     
- public:
- progress(const char* aff,const int& l): title(aff), length(l) {
-    t0 = clock();
-    prg=0; it=0;
-    std::cout << "\r";
-    std::cout << title << ": \t";
-    std::cout << prg << "%";
-    std::cout.flush(); }
-  
-  void operator++(int n){
-    it++;
-    //    assert(int(length/100)!=0);    
-    if( it%int(length/100) == 0){
-      std::cout << "\r";
-      std::cout << title << ": \t";;
-      std::cout << prg << "%";
-      if(prg==0){t0 = clock();}
-      prg++;
-      //      if(prg>100){std::cout << std::endl;}
-      std::cout.flush();
-    }  
-  }
-  
-  void end(){
-    t0 = clock()-t0;
-    time_t now; time(&now);
-    std::cout << "\r";
-    std::cout << title << ": \t";
-    std::cout << ((float)t0)/CLOCKS_PER_SEC << " sec." << std::endl;
-  }
-  
+private:
+    const char* title;
+    const int length;
+    int       it;
+    clock_t   t0;
+    
+public:
+    progress(const char* aff,const int& l): title(aff), length(l) {
+        t0 = clock();
+        it=0;
+
+        std::cout << "\r";
+        std::cout << title << ": \t";
+        std::cout << 0 << "%";
+        std::cout.flush(); }
+    
+    void operator++(int n){
+        it++;
+
+        if( it%int(length/100) == 0){
+            std::cout << "\r";
+            std::cout << title << ": \t";;
+            std::cout << int(it*100./length) << "%";
+            std::cout.flush();
+        }
+    }
+    
+    void end(){
+        t0 = clock()-t0;
+        time_t now; time(&now);
+        std::cout << "\r";
+        std::cout << title << ": \t";
+        std::cout << ((float)t0)/CLOCKS_PER_SEC << " sec." << std::endl;
+    }
+    
 };
 
 
