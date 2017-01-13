c227a2a5340dc1e1e77a39fb02bbdfec4f87f86f
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-01-13 17:53:53 +0100

    htool wrap ajouté

--- include/bemtool2/htool_wrap.h
+++ include/bemtool2/htool_wrap.h
@@ -2,9 +2,24 @@
 #define HTOOL_WRAP_H
 
 #include "htool.hpp"
-// #include "tools.h"
+#include "tools.h"
 
+template <class space, class kernel> class MyMatrix: public htool::VirtualMatrix{
+private:
+	potential<space,kernel>& Op;
+	const geometry& geom;
+	
+public:
+	MyMatrix(potential<space,kernel>& op,const geometry& geome, const int& nbpts, const int& nbdof): Op(op),geom(geome) {
+		nr = nbpts;
+		nc = nbdof;
+	}
 
+	const Cplx get_coef(const int& j, const int& k) const{
+// 		std::cout << get_node(geom,j) << std::endl;
+		return Op(get_node(geom,j),k);
+	}
+};
 
 
 #endif
