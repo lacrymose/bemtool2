55c521ea9261e2a8fd1c204875b33774332e6340
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-02-07 10:54:00 +0100

    Ball added in view.hpp

--- include/bemtool2/view.hpp
+++ include/bemtool2/view.hpp
@@ -11,6 +11,9 @@ void attach_ui(htool::Scene& s) {
 	// Variables
 	static Real lc= 0.1;
 	static Real R = 1 ;
+	enum enumeration {Disc,Ball};
+	static enumeration enumval = Disc;
+	bool enabled = true;
 	
 	// Scene (screen inside)
 	htool::statics& gv = htool::Scene::gv;
@@ -20,10 +23,12 @@ void attach_ui(htool::Scene& s) {
 	
 	// Window
 	nanogui::ref<nanogui::Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Bemtool - Disc");
-	nanoguiWindow->setPosition(Eigen::Vector2i(600, 200));
+	nanoguiWindow->setPosition(Eigen::Vector2i(500, 200));
 	
 	
 	// Parts of the window 
+	gui->addVariable("Mesh", enumval, enabled)
+           ->setItems({"Disc", "Ball"});
 	gui->addGroup("Parameters");
 	gui->addVariable("Mesh size", lc)->setSpinnable(true);
 	gui->addVariable("Radius", R)->setSpinnable(true);
@@ -32,51 +37,97 @@ void attach_ui(htool::Scene& s) {
 			std::cerr << "No active project" << std::endl;
 		else {
 			
-				gmsh_disc  ("disc",R,lc,0);
-				geometry vol;
-				load_node_gmsh(vol ,"disc");
-				mesh_2D Vol(vol);
-				load_elt_gmsh(Vol,0);
-
-				std::cout << "Mesh of a disc computed with radius "<< R<<" and mesh size "<<lc << std::endl;
+				std::vector<htool::R3> X;
+				std::vector<int> NbPts;
+				std::vector<htool::N4>  Elts;
+				std::vector<htool::R3> Normals;
 				
-				const vect<R3>& node = get_node(vol);
-				int nbnode  = size(node);
-				int nbelt = nb_elt(Vol);
+				switch(enumval){
+					case Disc:
+					{
+						gmsh_disc  ("disc",R,lc,0);
+						geometry vol;
+						load_node_gmsh(vol ,"disc");
+						mesh_2D Vol(vol);
+						load_elt_gmsh(Vol,0);
 
-				std::vector<htool::R3> X(nbnode);
-				std::vector<int> NbPts(nbelt);
-				std::vector<htool::N4>  Elts(nbelt);
-				std::vector<htool::R3> Normals(nbelt);
-
-				for (int i=0;i<nbnode;i++){
-					htool::R3 pt;pt[0]=node[i][0];pt[1]=node[i][1];pt[2]=node[i][2];
-					X[i]=pt;
-					
-				}
-				for (int i=0;i<nbelt;i++){
-					Elts[i][0]=&Vol[i][0]-&node[0];
-					Elts[i][1]=&Vol[i][1]-&node[0];
-					Elts[i][2]=&Vol[i][2]-&node[0];
-					Elts[i][3]=1;
-					
-					NbPts[i]=3;
-
-					Normals[i][0] = 0;
-					Normals[i][1] = 0;
-					Normals[i][2] = 1;
-					
-				}
-
-				htool::GLMesh m(X,Elts,NbPts,Normals);
+						std::cout << "Mesh of a disc computed with radius "<< R<<" and mesh size "<<lc << std::endl;
 				
-				s.set_mesh(m);
+						const vect<R3>& node = get_node(vol);
+						int nbnode  = size(node);
+						int nbelt = nb_elt(Vol);
+
+						X.resize(nbnode);
+						NbPts.resize(nbelt);
+						Elts.resize(nbelt);
+						Normals.resize(nbelt);
+
+						for (int i=0;i<nbnode;i++){
+							htool::R3 pt;pt[0]=node[i][0];pt[1]=node[i][1];pt[2]=node[i][2];
+							X[i]=pt;
+							
+						}
+						for (int i=0;i<nbelt;i++){
+							Elts[i][0]=&Vol[i][0]-&node[0];
+							Elts[i][1]=&Vol[i][1]-&node[0];
+							Elts[i][2]=&Vol[i][2]-&node[0];
+							Elts[i][3]=1;
+							
+							NbPts[i]=3;
+
+							Normals[i][0] = 0;
+							Normals[i][1] = 0;
+							Normals[i][2] = 1;
+							
+						}
+					}
+					case Ball:
+					{
+						gmsh_ball  ("ball",R,lc,0);
+						geometry vol;
+						load_node_gmsh(vol ,"ball");
+						mesh_2D Vol(vol);
+						load_elt_gmsh(Vol,0);
+
+						std::cout << "Mesh of a ball computed with radius "<< R<<" and mesh size "<<lc << std::endl;
+				
+						const vect<R3>& node = get_node(vol);
+						int nbnode  = size(node);
+						int nbelt = nb_elt(Vol);
+
+						X.resize(nbnode);
+						NbPts.resize(nbelt);
+						Elts.resize(nbelt);
+						Normals.resize(nbelt);
+
+						for (int i=0;i<nbnode;i++){
+							htool::R3 pt;pt[0]=node[i][0];pt[1]=node[i][1];pt[2]=node[i][2];
+							X[i]=pt;
+							
+						}
+						for (int i=0;i<nbelt;i++){
+							Elts[i][0]=&Vol[i][0]-&node[0];
+							Elts[i][1]=&Vol[i][1]-&node[0];
+							Elts[i][2]=&Vol[i][2]-&node[0];
+							Elts[i][3]=1;
+							
+							NbPts[i]=3;
+
+							Normals[i][0] = 0;
+							Normals[i][1] = 0;
+							Normals[i][2] = 1;
+							
+						}
+					}
 				
 // 				gv.active_project->set_ctrs(X);
 // 				gv.active_project->set_rays(Rays);
 		
 // 				gv.screen->performLayout();
-			
+				}
+				
+				htool::GLMesh m(X,Elts,NbPts,Normals);
+				s.set_mesh(m);
 		}
    });
 	
