d285ee31d2ecf97f0986b217ac632dbf646387d2
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-10-12 12:14:16 +0200

    cmake modules updated

--- cmake-modules/FindHtool.cmake
+++ cmake-modules/FindHtool.cmake
@@ -2,8 +2,8 @@
 # Try to find HPDDM library and include path.
 # Once done this will define
 #
-# HPDDM_FOUND
-# HPDDM_INCLUDE_DIR
+# HTOOL_FOUND
+# HTOOL_INCLUDE_DIRS
 #
 
 FIND_PATH(
@@ -16,7 +16,11 @@ FIND_PATH(
 # Handle the QUIETLY and REQUIRED arguments and set the HPDDM_FOUND to TRUE
 # if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Htool DEFAULT_MSG
+find_package_handle_standard_args(HTOOL DEFAULT_MSG
                                   HTOOL_INCLUDE_DIR)
 
 mark_as_advanced(HTOOL_INCLUDE_DIR)
+
+if (HTOOL_FOUND)
+    set(HTOOL_INCLUDE_DIRS ${HTOOL_INCLUDE_DIR} )
+endif()
